32c8163ff8aa89d1912b507cb5c5a8a6abfd2a4c
Franz <fana16y@qq.com>
2025-01-13 11:06:13 +0800

    CORS

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/project/",
+  base: "/franz.github.io/",
   build: {
     outDir: "../docs"
   },
